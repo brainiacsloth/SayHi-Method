619dc51fbb18d1b25f4feab7e5494ca7feb97c3b
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-05 11:55:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHi_Method
+{
+    internal class Program
+    {
+
+        static void Main(String[] args)
+        {
+            void sayHi()
+            {
+                Console.WriteLine("Hi");
+            }
+
+            void sayHiUser(string name)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+
+            void sayHiAge(string name, int age)
+            {
+                Console.WriteLine("Hello " + name + " you are " + age);
+            }
+
+            sayHi();
+            sayHiAge("John", 55);
+            sayHiUser("Evans");
+        }
+
+    }
+}
